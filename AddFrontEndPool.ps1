a4e0ed7a2e98c578193bf07288f1dc2e21d4b169
joshsumit <joshi.sumit@hotmail.com>
2017-05-02 18:02:47 +0530

    Add files via upload

---
+++ AddFrontEndPool.ps1
@@ -0,0 +1,12 @@
+﻿
+$resourceGroup = "myRG"
+$location =  "SouthIndia"
+$lb = Get-AzureRmLoadBalancer -Name 'mylb'  -ResourceGroupName $resourceGroup 
+$ip = New-AzureRmPublicIpAddress -Name 'myip3' -ResourceGroupName  $resourceGroup -Location $location
+
+Add-AzureRmLoadBalancerFrontendIpConfig -LoadBalancer $lb -PublicIpAddress $ip -Name 'feip'
+
+$fic = get-AzureRmLoadBalancerFrontendIpConfig -LoadBalancer $lb   -Name 'feip'
+
+
+Set-AzureRmLoadBalancer -LoadBalancer $lb  
